896e8a4c11abbcfc1ec54f00d8d1469f320e42fd
ez pawn <ezpawn@ezs-MacBook-Pro.local>
2024-07-11 16:07:42 +0300

    first commit initalinng the files

---
+++ object.js
@@ -0,0 +1,29 @@
+/*
+object => has methode , properteis
+ document.getElementById()object => has methode , properteis
+ document.getElementById()
+
+ ClassList => Add , remove ,toggle
+    element.classList.toggle()
+   
+  
+
+        wow.js => website
+
+
+Versio Controll => 
+    Word >
+        repository = Project that contain all files
+    Comands >
+        Clone : Bring files frome remote repo to Local Machin
+        add   : add files to Git for tracking changes
+        Commit: save files that has been changed
+        Push  : push/upload files to remote repo 'Github'
+        Pull  : pulling/dowload files that has been chang from remote repo to your machine 
+
+
+
+
+
+
+*/
