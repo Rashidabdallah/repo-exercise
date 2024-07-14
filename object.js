3df30f6f59e5ab336cb309416b79a8e1a75530f0
Rashid <rabdallah028@gmail.com>
2024-07-14 08:00:15 +0300

    inital of ice cream web page

--- object.js
+++ object.js
@@ -1,6 +1,6 @@
 /*
-object => has methode , properteis
- document.getElementById()object => has methode , properteis
+object => has method , properties
+ document.getElementById()object => has methods , properties
  document.getElementById()
 
  ClassList => Add , remove ,toggle
@@ -11,20 +11,26 @@ object => has methode , properteis
         wow.js => website
 
 
-Versio Controll => 
+Version Control => 
     Word >
-        repository = Project that contain all files
-    Comands >
-        Clone : Bring files frome remote repo to Local Machin
+        repository = Project that contain all files/folder
+    Commands >
+        Clone : Bring files from remote repo to Local Machine
         add   : add files to Git for tracking changes
         Commit: save files that has been changed
         Push  : push/upload files to remote repo 'Github'
         Pull  : pulling/download files that has been chang from remote repo to your machine 
 
 
-
+Animation =>
+    translate: X Y ;  move elements from stat to new coordinate X,Y
+    rotate: 180deg ;  rotate elements from stat to new orientation
+    scale: 1 ;        default value is '1' , < 1 = small element > 1 big element 
+                 All       1s         linear       1s
+    transition: property duration timing-function delay ;
+            -property to be transition , width / color.......
+            -duration how long it taKes to perform changes
 
 
 
 */
-alert('yes')
